ef33da4a0e09afa4fce1b926b6b640db97c6a13a
yoolalw <larakemilyprofissional@gmail.com>
2026-02-11 11:21:28 -0300

    finalizando logica da pagina home.html e iniciando a pagina de carrinho de compras

--- js/home.js
+++ js/home.js
@@ -1,46 +1,45 @@
-const produtos = [ //cria uma div que gera todos os produtos de formas diferentes
-    { //criacao da lista dos produtos
-        nomeKimono: "Kimono Rosa Floral",
-        precoKimono: 119.90,
-        imagem: "https://i.pinimg.com/736x/a9/be/2d/a9be2d5bd3afe8c88d6704c8b550395f.jpg"
-    }, 
-    {
-        nomeKimono: "Kimono azul",
-        precoKimono: 120.00,
-        imagem: "https://i.pinimg.com/736x/98/79/a1/9879a145d580556d14991fb16d27fc59.jpg"
-    }
-]
-
-
 const container = document.getElementById("produto"); // cria um container pra ficar recriando sempre que entrar um novo produto dentro do estoque
 
-produtos.forEach(produto => {
-    container.innerHTML += `
-        <div class="image-content">
-            <img src="${produto.imagem}" style="width:100%; height:80%; object-fit:cover; border-radius: 10px;">
+async function carregarProdutos() {
+    try{
+        const response = await fetch('http://localhost:8081/users/register');
+        const produtos = await response.json();
 
-            <h3 style="text-align:left; margin-left: 11px; font-size: 22px; margin-top: 10px;">${produto.nomeKimono}</h3>
+        produtos.forEach(produto => {
+            const div = document.createElement("div");
+            div.classList.add("image-content");
+            div.innerHTML = `
+                    <img src="${produto.imagem}" style="width:100%; height:80%; object-fit:cover; border-radius: 10px;">
+
+                    <h3 style="text-align:left; margin-left: 11px; font-size: 22px; margin-top: 10px;">${produto.nomeKimono}</h3>
+                    
+                    <p style="text-align:left; margin-left: 11px; font-size: 22px; margin-top: 0px;">R$ ${produto.precoKimono.toFixed(2)}</p>
+
+                    <button class="cart-btn-img" onclick="adicionarAoCarrinho()">🛒</button>
+                    
+
+                </div>
+            `;
+            container.appendChild(div);
             
-            <p style="text-align:left; margin-left: 11px; font-size: 22px; margin-top: 0px;">R$ ${produto.precoKimono.toFixed(2)}</p>
-
-            <button class="cart-btn-img" onclick="adicionarAoCarrinho()">🛒</button>
             
+            const btnProduto = div.querySelector(".cart-btn-img");
+            btnProduto.onclick = () => {
+                alert(produto.nomeKimono + " adicionado ao carrinho!");
+            };
+        });
 
-        </div>
-    `;
+            const btnHeader = document.getElementById("cart-button-header");
+            btnHeader.onclick = () =>
+            {
+               window.open("http://127.0.0.1:5500/BloomsKimonoWeb/carrinho.html"); //sempre que clicar no botao, vai abrir essa pag}
+            };
+        }catch(error){
+            console.error("Erro ao carregar os produtos", error);
+            container.innerHTML = "<p> Erro ao carregar os produtos. </p>";
+
+        }
+    }
+carregarProdutos();
 
 
-});
-
-
-
-
-function adicionarAoCarrinho(nomeKimono){
-    alert(nomeKimono + "foi adicionado ao carrinho!");
-}
-
-const btn = document.getElementById("cart-button-header"); //declara o botao
-
-    btn.onclick = () => { //botao.evento (onclick)
-        window.open("http://127.0.0.1:5500/BloomsKimonoWeb/carrinho.html"); //sempre que clicar no botao, vai abrir essa pag}
-}
