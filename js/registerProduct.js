6ccfefe7a28fdbb00bcbc3d8a3afd2181948037a
yoolalw <larakemilyprofissional@gmail.com>
2026-03-31 12:20:02 -0300

    aa

--- js/registerProduct.js
+++ js/registerProduct.js
@@ -1,10 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js"
 
-const supabase = createClient('https://supabase.com/dashboard/project/neyrjxcpalyrewvwyoui/storage/files/buckets/products',
+const supabase = createClient('https://neyrjxcpalyrewvwyoui.supabase.co',
     'sb_publishable_V1-gtZmUOULjpLNML9DPzA_FhUO1dj3')
-
-
-console.log(supabase);
 
 export const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
@@ -12,36 +9,28 @@ export const corsHeaders = {
 };
 
 const message = document.getElementById("message");
-
 const registerProductForm = document.querySelector("#registerProductForm")
-const formData = new FormData(registerProductForm);
 
 registerProductForm.addEventListener('submit', async function (event) {
     event.preventDefault();
-
+    
+    const formData = new FormData(registerProductForm);
     async function registerProduct() {
-        const nomeKimono = document.getElementById("nomeKimono").value;
-        const precoKimono = document.getElementById("precoKimono").value;
-        const quantidadeKimono = document.getElementById("quantidadeKimono").value;
-        const imagem = document.getElementById("imagem").files[0];
 
-        formData.append('nomeKimono', nomeKimono);
-        formData.append('precoKimono', precoKimono);
-        formData.append('quantidadeKimono', quantidadeKimono);
-        formData.append('imagem', imagem);
         
-        
+    const nomeKimono = document.getElementById("nomeKimono").value;
+    const precoKimono = document.getElementById("precoKimono").value;
+    const quantidadeKimono = document.getElementById("quantidadeKimono").value;
+    const imagem = document.getElementById("imagem").files[0];
 
-// Create a single supabase client for interacting with your database
-        
+    console.log(nomeKimono)
+
         try{
-        const { error } = await supabase
-            .from('imagem')
-            .insert({imagem})
+        const { data, error } = await supabase.from('products').insert()
         } catch (error){
             return error;
         }
-
+    
         try {
             const reponse = await fetch('http://localhost:8080/products/registerProd', {
                 method: 'POST',
@@ -53,13 +42,11 @@ registerProductForm.addEventListener('submit', async function (event) {
             if (reponse.ok) {
                 message.innerHTML = "Conexao realizada"
 
-
             } else {
                 const jsonResponse = reponse.json();
                 console.log(jsonResponse);
                 message.innerHTML = "Ocorreu algum erro ao tentar cadastrar"
             }
-            createClient
 
 
         } catch (error) {
