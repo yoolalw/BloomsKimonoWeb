9d3f007d45b9acf1a72626ad104c00388a891cf8
yoolalw <larakemilyprofissional@gmail.com>
2026-04-21 18:56:40 -0300

    cart quase finalizado // reler e anotar todas as modificacoes do cart

---
+++ js/cart.js
@@ -0,0 +1,58 @@
+const cartContainer = document.querySelector("#cartContainer")
+
+const urlFetchCartItens = 'http://localhost:8080/cart'
+console.log(urlFetchCartItens)
+
+async function getCartItens() {
+    try {
+        const response = await fetch(urlFetchCartItens)
+        console.log(response.status)
+
+        if (response.ok) {
+            const cartItem = await response.json()
+            console.log(JSON.stringify(cartItem))
+
+            cartItem.forEach((item) => {
+                cartContainer.innerHTML += `
+                <div class="containerInn">
+                    <div class="itemSessInn" id="itemSessInn-${item.idCartItem}">
+                        <img src="${item.productModel.imagem}">
+                        <p>${item.productModel.nomeKimono}</p>
+                        <p>${item.productModel.precoKimono}</p>
+                        <p>${item.total}</p>
+            
+                        <button type="button" data-id="${item.idCartItem}" class="removeBtn">Remover</button>
+                    </div>
+                </div>
+                `
+
+                const btn = cartContainer.querySelector(`[data-id="${item.idCartItem}"]`)
+                btn.addEventListener('click', async () => {
+                    const id = btn.dataset.id
+
+                    try {
+                        const response = await fetch(`http://localhost:8080/cart/${id}`, {
+                            method: "DELETE",
+                            headers: { 'Content-Type': 'application/json' }
+                        })
+
+                        if (response.ok) {
+                            alert("Item removido com sucesso!")
+                            document.getElementById(`itemSessInn-${id}`).remove()
+                        } else {
+                            throw new Error("erro! ::::: ")
+                        }
+
+                    } catch (error) {
+                        console.log("ERROR!! ::", error)
+                    }
+                })
+            });
+
+        } else {
+            throw new Error("erro! ::::: ")
+        }
+    } catch (error) {
+        console.error("ERROR!! : ", error)
+    }
+} getCartItens()
