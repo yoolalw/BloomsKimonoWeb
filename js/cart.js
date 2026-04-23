04935f6e07d6063c97b295f28307f1d6607db573
yoolalw <larakemilyprofissional@gmail.com>
2026-04-23 16:58:10 -0300

    remove botao // finalizar calculo de total

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,5 @@
 const cartContainer = document.querySelector("#cartContainer")
+const message = document.getElementById("message")
 
 const urlFetchCartItens = 'http://localhost:8080/cart'
 console.log(urlFetchCartItens)
@@ -23,17 +24,23 @@ async function getCartItens() {
                         <button type="button" 
                             data-id="${item.idCartItem}" 
                             data-quantidade-item="${item.quantidadeItem}" 
+                            data-valor-item="${item.productModel.precoKimono}"
                             id="addQuant-${item.idCartItem}"
                             class="addQuantBtn">+
-                            </button>
+                        </button>
 
                         <p id="quant-${item.idCartItem}">${item.quantidadeItem}</p>
                         
                         <button type="button" 
                             data-id="${item.idCartItem}" 
+                            data-quantidade-item="${item.quantidadeItem}" 
+                            data-valor-item="${item.productModel.precoKimono}"
+                            id="removeQuantBtn-${item.idCartItem}"
                             class="removeQuantBtn">-</button>
 
-                        <p>Total: ${item.total}</p>
+                        <p class="total"
+                            id="total"
+                            >Total: ${item.total}</p>
             
                         <button type="button" 
                             data-id="${item.idCartItem}" 
@@ -42,12 +49,12 @@ async function getCartItens() {
                     </div>
                 </div>
                 `)
-                // Deletando itens do carrinho!!!!
 
                 const novoItem = cartContainer.lastElementChild
                 const btn = novoItem.querySelector('.removeBtn')
                 const addQuantBtn = novoItem.querySelector('.addQuantBtn')
                 const removeQuantBtn = novoItem.querySelector('.removeQuantBtn')
+                const total = novoItem.querySelector(".total")
 
                 btn.addEventListener('click', async (e) => {
                     e.preventDefault()
@@ -70,14 +77,19 @@ async function getCartItens() {
                         console.log("ERROR!! ::", error)
                     }
                 })
-                //Atualizando quantidade de produtos do carrinho!!!!
+                //Finaliza funcao de remover item do carrinho
 
+                //Atualizando quantidade de produtos do carrinho!!!!
                 addQuantBtn.addEventListener('click', async (e) => {
                     e.preventDefault()
 
                     const quantidadeAtual = Number(addQuantBtn.dataset.quantidadeItem)
+                    const valorItem = Number(addQuantBtn.dataset.valorItem)
                     const quantNova = quantidadeAtual + 1
+                    const calcValue = valorItem * quantNova
                     const id = addQuantBtn.dataset.id
+
+                    removeQuantBtn.disabled = false
 
                     try {
 
@@ -86,7 +98,7 @@ async function getCartItens() {
                             headers: {
                                 "Content-Type": "application/json"
                             },
-                            body: JSON.stringify({ quantidadeItem: quantNova })
+                            body: JSON.stringify({ quantidadeItem: quantNova, total: calcValue })
                         })
 
                         if (response.ok) {
@@ -103,6 +115,47 @@ async function getCartItens() {
                         console.error(error)
                     }
                 });
+                //Finaliza funcao de adicionar mais quantidade
+
+                removeQuantBtn.addEventListener('click', async (e) => {
+                    e.preventDefault()
+
+                    const quantidadeAtual = Number(addQuantBtn.dataset.quantidadeItem)
+                    const valorItem = Number(addQuantBtn.dataset.quantidadeItem)
+                    const quantRemov = quantidadeAtual - 1
+                    const calcValue = valorItem * quantRemov
+                    const id = removeQuantBtn.dataset.id
+
+                    removeQuantBtn.disabled = (quantidadeAtual == 1);
+                    if(quantidadeAtual > 1) { 
+                        removeQuantBtn.disabled = false
+                    }
+
+                    try {
+
+                        const response = await fetch(`http://localhost:8080/cart/${id}`, {
+                            method: "PUT",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify({ quantidadeItem: quantRemov, total: calcValue })
+                        })
+
+                        if (response.ok) {
+                            document.getElementById(`quant-${id}`).textContent = quantRemov
+                            addQuantBtn.dataset.quantidadeItem = quantRemov
+                            
+                           
+                        }
+
+                    } catch (error) {
+                        throw new Error(error)
+                    }
+                })
+
+             
+                
+
             });
         } else {
             throw new Error("erro! ::::: ")
