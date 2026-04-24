ae24e883c297d1eec4663434db8e3580f1b24ab1
yoolalw <larakemilyprofissional@gmail.com>
2026-04-24 11:05:23 -0300

    adicionando botao de remover na home

--- js/home.js
+++ js/home.js
@@ -13,15 +13,49 @@ async function getProductsFromDb() {
             console.log(dados)
 
             dados.forEach((dataDb) => {
-                produto.innerHTML += `
+                produto.insertAdjacentHTML('beforeend', `
                 <a href="detailsProduct.html?id=${dataDb.id}">
                 <div class="prodSessInn" id="prodSessInn-${dataDb.id}">
                     <img src="${dataDb.imagem}">
+                    </a>
                     <p>${dataDb.nomeKimono}</p>
                     <h3>${dataDb.precoKimono}</h3>
+
+                     <button type="button"
+                        data-id="${dataDb.id}"
+                        class="removeProd"
+                        >Deletar
+                    </button>
                 </div>  
-                </a>
-                `
+                `)
+
+                const decProd = produto.lastElementChild
+                const removeProd = decProd.querySelector(".removeProd")
+
+                removeProd.addEventListener("click", async (e) => {
+                    e.preventDefault()
+                    const id = removeProd.dataset.id
+                    console.log(id)
+
+                    try {
+                        const response = await fetch(`http://localhost:8080/products/${id}`, {
+                            method: "DELETE",
+                            headers: {
+                                "Content-Type": "application/json"
+                            }
+                        })
+                        if (response.ok) {
+                            document.getElementById(`prodSessInn-${dataDb.id}`).remove()
+
+                        } else {
+                            throw new Error();
+                        }
+                    } catch (err) {
+                        console.error("Erro!! ::: ", err)
+                    }
+
+                });
+
             });
         }
     } catch (err) {
