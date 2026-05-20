764e9e02695c3f30bf14b6e2e97563b0a3562c33
yoolalw <larakemilyprofissional@gmail.com>
2026-05-20 14:26:24 -0300

    finalizando test_register_page.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,15 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="function", autouse=True)
 def chrome(request):
     service = Service(ChromeDriverManager().install())
     chrome = webdriver.Chrome(service=service)
-    chrome.get("http://127.0.0.1:5500/BloomsKimonoWeb/register.html")
+    chrome.get("http://127.0.0.1:5500/BloomsKimonoWeb")
     chrome.implicitly_wait(5)
     chrome.maximize_window()
 
-    request.cls = chrome
+    request.cls.chrome = chrome
 
     yield chrome
     chrome.quit()
--- tests/test_register_page.py
+++ tests/test_register_page.py
@@ -1,5 +1,6 @@
 import time
-import conftest
+
+from conftest import chrome
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -9,9 +10,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 @pytest.mark.usefixtures("chrome")
 class TestInterfaceRegisterPage:
     chrome: WebDriver
+
+    def setup_method(self):
+        self.chrome.get("http://127.0.0.1:5500/BloomsKimonoWeb/register.html")
 
     def test_register_elements_displayed(self):
         self.chrome.find_element(By.ID, "nomeUser").is_displayed()
@@ -21,13 +26,13 @@ class TestInterfaceRegisterPage:
         self.chrome.find_element(By.ID, "btnRegister").is_displayed()
 
     def test_inserting_itens_in_elements_and_checking_message(self):
-        self.chrome.find_element(By.ID, "nomeUser").send_keys("userNameForTesting")
-        self.chrome.find_element(By.ID, "emailUser").send_keys("emailForTesting@gmail.com")
+        self.chrome.find_element(By.ID, "nomeUser").send_keys("u serNameForTesting")
+        self.chrome.find_element(By.ID, "emailUser").send_keys("passingEmailsForTest@gmail.com")
         self.chrome.find_element(By.ID, "senhaUser").send_keys("passForTest")
         self.chrome.find_element(By.ID, "confSenhaUser").send_keys("passForTest")
         self.chrome.find_element(By.ID, "btnRegister").click()
 
-        time.sleep(4)
+        time.sleep(2)
 
         message = self.chrome.find_element(By.ID, "message")
         assert message.text == 'Registro enviado!'
@@ -39,10 +44,9 @@ class TestInterfaceRegisterPage:
             EC.url_to_be("http://127.0.0.1:5500/BloomsKimonoWeb/login.html")
         )
 
-
     def test_inserting_itens_and_redirect(self):
         self.chrome.find_element(By.ID, "nomeUser").send_keys("userNameForTesting")
-        self.chrome.find_element(By.ID, "emailUser").send_keys("emailForTesting2@gmail.com")
+        self.chrome.find_element(By.ID, "emailUser").send_keys("emailFForTeeestinNg222222@gmail.com")
         self.chrome.find_element(By.ID, "senhaUser").send_keys("passForTest")
         self.chrome.find_element(By.ID, "confSenhaUser").send_keys("passForTest")
         self.chrome.find_element(By.ID, "btnRegister").click()
@@ -56,11 +60,10 @@ class TestInterfaceRegisterPage:
             EC.url_to_be("http://127.0.0.1:5500/BloomsKimonoWeb/home.html")
         )
 
-
     def test_not_insert_same_password(self):
         self.chrome.find_element(By.ID, "nomeUser").send_keys("userNameForTesting")
 
-        self.chrome.find_element(By.ID, "emailUser").send_keys("emailForTesting@gmail.com")
+        self.chrome.find_element(By.ID, "emailUser").send_keys("emeailForTesting@gmail.com")
         self.chrome.find_element(By.ID, "senhaUser").send_keys("passForTest")
         self.chrome.find_element(By.ID, "confSenhaUser").send_keys("wrongPassForTest")
 
@@ -69,8 +72,8 @@ class TestInterfaceRegisterPage:
         message = self.chrome.find_element(By.ID, "message")
         assert message.text == "As senhas nao coincidem!"
 
-        def test_field_name_null(self):
-            self.chrome.find_element(By.ID, "emailUser").send_keys("emailForTesting@gmail.com")
+    def test_field_name_null(self):
+        self.chrome.find_element(By.ID, "emailUser").send_keys("emailForTesting@gmail.com")
 
         self.chrome.find_element(By.ID, "senhaUser").send_keys("passForTest")
         self.chrome.find_element(By.ID, "confSenhaUser").send_keys("passForTest")
@@ -79,7 +82,6 @@ class TestInterfaceRegisterPage:
 
         message = self.chrome.find_element(By.ID, "message")
         assert message.text == "Campo nome vazio!"
-
 
     def test_field_email_null(self):
         self.chrome.find_element(By.ID, "nomeUser").send_keys("userNameForTesting")
@@ -91,7 +93,6 @@ class TestInterfaceRegisterPage:
         message = self.chrome.find_element(By.ID, "message")
         assert message.text == "Campo email vazio!"
 
-
     def test_field_password_null(self):
         self.chrome.find_element(By.ID, "nomeUser").send_keys("userNameForTesting")
         self.chrome.find_element(By.ID, "emailUser").send_keys("emailForTesting@gmail.com")
@@ -100,7 +101,6 @@ class TestInterfaceRegisterPage:
 
         message = self.chrome.find_element(By.ID, "message")
         assert message.text == "Campos de senha vazio!"
-
 
     def test_insert_exists_email(self):
         self.chrome.find_element(By.ID, "nomeUser").send_keys("userNameForTesting")
