47c54ad13ebddafcba88f225581d0192b5d44cfa
yoolalw <larakemilyprofissional@gmail.com>
2026-07-17 11:20:31 -0300

    passando os testes pra pagina home pegando xpath especifico e alert

--- tests/pages/page_home.py
+++ tests/pages/page_home.py
@@ -1,4 +1,6 @@
+import allure
 import pytest
+from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 
@@ -6,17 +8,68 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 from tests.conftest import driver
 
+
 class HomePage:
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(self.driver, 10)
+        self.card = (By.XPATH, '//*[@class="prodSessInn"]')
+        self.card_img = (By.XPATH, '//*[@class="prodSessInn"]/a/img')
+        self.card_title = (By.XPATH, '//*[@class="prodSessInn"]/p')
+        self.card_price = (By.XPATH, '//*[@class="prodSessInn"]/h3')
+        self.delete_button = (By.CLASS_NAME, 'removeProd')
+        self.edit_button = (By.XPATH, '//*[@class="prodSessInn"]/a[2]/button')
 
-        self.card = (By.CLASS_NAME, 'prodSessInn/id')
+        self.cadastrar_prod =  (By.XPATH, '/html/body/a')
+        self.cart_btn = (By.XPATH, '/html/body/div[1]/a[2]')
 
 
+    @allure.step("Verificando comportamento dos elementos no card")
+    def verificando_comportamento_dos_elementos_no_card(self):
+        return self.wait.until(expected_conditions.visibility_of_element_located(self.card_img)) and \
+            self.wait.until(expected_conditions.visibility_of_element_located(self.card_title)) and \
+            self.wait.until(expected_conditions.visibility_of_element_located(self.card_price)) and \
+            self.wait.until(expected_conditions.visibility_of_element_located(self.delete_button)) and \
+            self.wait.until(expected_conditions.visibility_of_element_located(self.edit_button))
 
+    @allure.step("Clicando no botao de deletar produto")
+    def click_deletar_produto(self):
+        self.wait.until(expected_conditions.visibility_of_element_located(self.delete_button)).click()
 
+    @allure.step("Clicando no botao de editar produto")
+    def click_editar_produto(self):
+        self.wait.until(expected_conditions.visibility_of_element_located(self.edit_button)).click()
 
+    @allure.step("Clicando no card")
+    def click_card(self):
+        self.wait.until(expected_conditions.visibility_of_element_located(self.card)).click()
 
+    @allure.step("Verificando redirecionamento para tela de detalhes")
+    def redirect_detalhes_page(self):
+        self.wait.until(expected_conditions.url_to_be('http://127.0.0.1:5500/detailsProduct.html'))
 
+    @allure.step("Verificando redirecionamento para tela de editar produto")
+    def redirect_editar_page(self):
+        self.wait.until(expected_conditions.url_to_be('http://127.0.0.1:5500/updateProduct.html?id=1002'))
 
+    @allure.step("Verificando se o produto foi deletado")
+    def verificando_se_o_item_foi_removido(self):
+        alert = Alert(self.driver)
+        self.wait.until(expected_conditions.alert_is_present())
+        alert.accept()
+
+    @allure.step("Clicando no botao de cadastrar novos produtos")
+    def click_cadastrar_produto(self):
+        self.wait.until(expected_conditions.visibility_of_element_located(self.cadastrar_prod)).click()
+
+    @allure.step("Verificando url da pagina de cadastro de produtos")
+    def redirect_cadastro_de_produto(self):
+        return self.wait.until(expected_conditions.url_to_be('http://127.0.0.1:5500/registerProduct.html'))
+
+    @allure.step("Clicando no botao de ver carrinho")
+    def click_ver_carrinho(self):
+        self.wait.until(expected_conditions.visibility_of_element_located(self.cart_btn)).click()
+
+    @allure.step("Verificando redirecionamento para pagina de ver carrinho")
+    def redirect_ver_carrinho(self):
+        return self.wait.until(expected_conditions.url_to_be('http://127.0.0.1:5500/cart.html'))
--- tests/test_pages/test_home_page.py
+++ tests/test_pages/test_home_page.py
@@ -1,4 +1,6 @@
+import allure
 import pytest
+from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions
@@ -7,6 +9,11 @@ from selenium.webdriver.support.wait import WebDriverWait
 from tests.conftest import driver
 from tests.pages.page_home import HomePage
 
+
+@allure.parent_suite("Teste web")
+@allure.feature("Testes na tela inicial")
+@allure.description(
+    "Os testes nessa página irão priorizar a criação de produtos e facil dominio para vendedores de kimonos")
 @pytest.mark.usefixtures("driver")
 class TestHomePage:
     driver = WebDriver
@@ -15,8 +22,32 @@ class TestHomePage:
     def setup_method(self, driver):
         self.driver.get('http://127.0.0.1:5500/home.html')
         self.home_page = HomePage(self.driver)
-# //*[@id="prodSessInn-1002"]
-    def test_item(self):
-        prod = self.driver.find_element(By.XPATH, '//[@class="prodSessInn"]')
-        print(prod)
 
+    @allure.title("Existencia dos elementos no card")
+    def test_verificando_existencia_dos_elementos_no_card(self):
+        assert self.home_page.verificando_comportamento_dos_elementos_no_card()
+
+    @allure.title("Clicando no botao de deletar")
+    def test_deletando_produtos_e_verificando_alerta(self):
+        self.home_page.click_deletar_produto()
+        self.home_page.verificando_se_o_item_foi_removido()
+
+    @allure.title("Clicando no botao de editar produto")
+    def test_clicando_em_editar_produto_e_verificando_redirecionamento(self):
+        self.home_page.click_editar_produto()
+        assert self.home_page.redirect_editar_page()
+
+    @allure.title("Clicando no card")
+    def test_clicando_no_card_e_redirecionando_para_pagina_de_detalhes(self):
+        self.home_page.click_card()
+        assert self.home_page.redirect_detalhes_page()
+
+    @allure.title("Clicando no botao de cadastro de produtos")
+    def test_clicando_no_botao_de_cadastro_de_produtos(self):
+        self.home_page.click_cadastrar_produto()
+        assert self.home_page.redirect_cadastro_de_produto()
+
+    @allure.title("Clicando no botao de ver carrinho")
+    def test_clicando_no_botao_de_ver_carrinho(self):
+        self.home_page.click_ver_carrinho()
+        assert self.home_page.redirect_ver_carrinho()
